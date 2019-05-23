e95af16d5f1bbaebd33bc72d53f6b5d349b0bd9b
Oleksandr Kalishchuk <50943310+OleksandrKalishchuk@users.noreply.github.com>
2019-05-23 10:26:59 +0300

    Параметры
    Здесь не нужен модуль инфоблоков и их получение.
    Типа TEXT нету, есть STRING.
    Параметр MULTIPLE по умолчанию N, поэтому можно упустить.
    В component.php используется кеш, примере он так же указан, поэтому нужен параметр CACHE_TIME

--- flamix/preloader/.parameters.php
+++ flamix/preloader/.parameters.php
@@ -1,16 +1,4 @@
-<?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-CModule::IncludeModule("iblock");
-
-$dbIBlockType = CIBlockType::GetList(
-    array("sort" => "asc"),
-    array("ACTIVE" => "Y")
-);
-while ($arIBlockType = $dbIBlockType->Fetch())
-{
-    if ($arIBlockTypeLang = CIBlockType::GetByIDLang($arIBlockType["ID"], LANGUAGE_ID))
-        $arIblockType[$arIBlockType["ID"]] = "[".$arIBlockType["ID"]."] ".$arIBlockTypeLang["NAME"];
-}
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 $arComponentParameters = array(
     "GROUPS" => array(
@@ -22,25 +10,21 @@ $arComponentParameters = array(
         "BG_COLOR" => array(
             "PARENT" => "PARAMS",
             "NAME" => 'Background color',
-            "TYPE" => "TEXT",
-            "MULTIPLE" => "N",
+            "TYPE" => "STRING",
             "DEFAULT" => "#121212"
         ),
         "MAIN_COLOR" => array(
             "PARENT" => "PARAMS",
             "NAME" => 'Main X color',
-            "TYPE" => "TEXT",
-            "MULTIPLE" => "N",
+            "TYPE" => "STRING",
             "DEFAULT" => "#fff"
         ),
         "SECOND_COLOR" => array(
             "PARENT" => "PARAMS",
             "NAME" => 'Second X color',
-            "TYPE" => "TEXT",
-            "MULTIPLE" => "N",
+            "TYPE" => "STRING",
             "DEFAULT" => "#46B03D"
         ),
-
+        "CACHE_TIME" => array()
     ),
 );
-?>
